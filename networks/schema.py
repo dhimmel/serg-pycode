cabbaafe894dcdc4faeb71dec6a4d65ee63eb023
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2012-12-04 18:00:09 -0800

    Added preliminary to compute path count for a given metapath.

--- networks/schema.py
+++ networks/schema.py
@@ -6,6 +6,7 @@ class Schema(object):
     def __init__(self, g, node_kinds, edge_tuples):
         """Create a graph schema representing node and edge kinds. Meant for
         either directed or undirected multigraphs."""
+        self.edge_tuples = edge_tuples
         self.g = g
         g.graph['kind'] = 'schema'
         g.add_nodes_from(node_kinds)
@@ -13,12 +14,27 @@ class Schema(object):
         self.edge_kinds = set()
         for source, target, kind in edge_tuples:
             self.edge_kinds.add(kind)
+            assert source in g and target in g
             g.add_edge(source, target, key=kind)
         
     def plot(self):
         networkx.draw_circular(self.g)
         matplotlib.pyplot.show()
     
+    def __str__(self):
+        edge_list = list()
+        for edge_tuple in self.edge_tuples:
+            edge = [edge_tuple[0], edge_tuple[2], edge_tuple[1]]
+            edge_list.append(self.path_as_str(edge))
+        return 'Nodes: ' + ', '.join(self.node_kinds) + '\nEdges:\n' + '\n'.join(edge_list)
+    
+    @staticmethod
+    def path_as_str(path):
+        path = list(path)
+        for i in range(len(path)):
+            if i % 2:
+                path[i] = ' <--' +  path[i] + '--> '
+        return ''.join(path)
     
 class UndirectedSchema(Schema):
     
@@ -45,12 +61,57 @@ class UndirectedSchema(Schema):
         return paths
 
 
+def path_count(g, source, target, metapath):
+    """ """
+    assert g.node[source]['kind'] == metapath[0]
+    assert g.node[target]['kind'] == metapath[-1]
+    metapath.pop(0)
+    paths = [[source]]
+    while metapath:
+        edge_key = metapath.pop(0)
+        node_kind = metapath.pop(0)
+        current_depth_paths = list()
+        while paths:
+            preceeding_path = current_depth_paths.pop()
+            node = preceeding_path[-1]
+            
+            for node, neighbor, key in g.edges(node, keys=True):
+                neighbor_kind = g.node[neighbor]['kind']
+                if key == edge_key and neighbor_kind == node_kind:
+                    path = preceeding_path + [edge_key, neighbor]
+                    current_depth_paths.append(path)
+        paths = current_depth_paths
+    paths_from_source = len(paths)
+    paths = filter(lambda path: path[-1] == target, paths)
+    return len(paths), paths
 
 if __name__ == '__main__':
     node_kinds = {'drug', 'disease', 'gene'}
+    """
     edge_tuples = [('drug', 'gene', 'target'),
                    ('gene', 'disease', 'risk'),
                    ('gene', 'gene', 'function')]
+    """
+    edge_tuples = [('drug', 'gene', 'target'),
+                   ('gene', 'disease', 'risk'),
+                   ('drug', 'disease', 'indication')]
+
+    
+    """
+    edge_tuples = [('drug', 'gene', 'upregulates'),
+                   ('drug', 'gene', 'downregulates'),
+                   ('disease', 'gene', 'upregulates'),
+                   ('disease', 'gene', 'downregulates'),
+                   ('gene', 'gene', 'function')]
+    """
+    
     omicnet_schema = UndirectedSchema(node_kinds, edge_tuples)
     #print omicnet_schema.g.edges('gene')
-    print list(omicnet_schema.metapaths('drug', 'disease'))
+    metapaths = omicnet_schema.metapaths('drug', 'disease', 3)
+    metapath_strs = [Schema.path_as_str(path) for path in metapaths]
+    print omicnet_schema
+    print 'Metapaths with length <= 3:'
+    for metapath in metapaths:
+        print Schema.path_as_str(metapath)
+        
+        
