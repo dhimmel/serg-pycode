adebb55c28251b02a0c2b76c059995750ac540d7
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2013-03-25 21:01:59 -0700

    Rewrote heteronets.schema.MetaPath() to define __new__ so existing metapaths are returned instead of duplicated. MetaPaths() class no longer needed and removed. Other files besides schema.py still need to be updated to reflect this change.

--- heteronets/schema.py
+++ heteronets/schema.py
@@ -24,7 +24,7 @@ def create_undirected_schema(edges, kind_to_abbrev):
         schema.add_edge(edge[0], edge[1], key=edge[2])
     
     check_abbreviations(schema, kind_to_abbrev)
-    schema.graph['paths'] = MetaPaths(schema, kind_to_abbrev)
+    MetaPath.kind_to_abbrev = kind_to_abbrev
     
     return schema
 
@@ -42,7 +42,7 @@ def print_schema(schema):
         print edge
     
     print 'Kind Abbreviations'
-    kind_to_abbrev = schema.graph['paths'].kind_to_abbrev
+    kind_to_abbrev = MetaPath.kind_to_abbrev
     for kind, abbrev in kind_to_abbrev.items():
         print kind + ': ' + abbrev
 
@@ -73,10 +73,8 @@ def extract_metapaths(schema, source, target, max_length,
     
     assert isinstance(exclude_edges, set)
     assert all(isinstance(exclude_edge, tuple) for exclude_edge in exclude_edges)
-    
-    paths_obj = schema.graph['paths']
-        
-    potential_paths = [paths_obj.metapath_from_tuple((source, ))]
+            
+    potential_paths = [MetaPath((source, ))]
     paths = list()
     for depth in range(max_length):
         current_depth_paths = list()
@@ -92,7 +90,7 @@ def extract_metapaths(schema, source, target, max_length,
                     (neighbor, node, key) in exclude_edges):
                     continue
                 
-                path = paths_obj.append_to_metapath(potential_path, key, neighbor)
+                path = potential_path.append(key, neighbor)
                 
                 current_depth_paths.append(path)
                 if neighbor == target:
@@ -100,109 +98,104 @@ def extract_metapaths(schema, source, target, max_length,
         potential_paths = current_depth_paths
     return paths
 
-def shortcuts_for_metapaths(schema, metapaths, shortcut_length):
-    """Compute desired shortcuts for faster computation of metapaths."""
-    shorcuts = set()
-    for metapath in metapaths:
-        num_nodes = len(metapath)
-        depth = 0
-        while depth + shortcut_length <= num_nodes:
-            start = depth * 2
-            end = start + shortcut_length * 2 + 1
-            shortcut = metapath.tuple_[start:end]
-            shortcut = schema.graph['paths'].metapath_from_tuple(shortcut)
-            shorcuts.add(shortcut)
-            depth += shortcut_length
-    return shorcuts
-
-def edges_in_metapaths(metapaths):
-    """Returns a set of edges appearing in any of the metapaths"""
-    edges = set()
-    for metapath in metapaths:
-        edges |= metapath.get_edges()
-    return edges
-
-class MetaPaths(object):
-    
-    def __init__(self, schema, kind_to_abbrev):
-        self.schema = schema
-        
-        self.abbrev_to_path = dict()
-        #self.paths = set()
-        #self.tuple_to_path = dict()
-        
-        self.kind_to_abbrev = kind_to_abbrev
-        self.abbrev_to_kind = {value: key for key, value in self.kind_to_abbrev.iteritems()}
-        
-    def metapath_from_abbrev(self, abbrev):
-        """Return the metapath represented by the supplied abbreviation."""
-        metapath = self.abbrev_to_path.get(abbrev)
-        if metapath is None:
-            kind_tuple = tuple(self.abbrev_to_kind[x] for x in abbrev)
-            metapath = MetaPath(self, kind_tuple)
-        return metapath
-    
-    def metapath_from_tuple(self, kind_tuple):
-        """Return the metapath represented by the supplied tuple of kinds."""
-        abbrev = ''.join(self.kind_to_abbrev[kind] for kind in kind_tuple)
-        return self.metapath_from_abbrev(abbrev)
-        
-    def append_to_metapath(self, metapath, edge_kind, node_kind,
-                           abbreviated=False, prepend=False):
-        """
-        Return the metapath representing the supplied metapath appended with
-        the supplied edge_kind and node_kind. abbreviated indicates whether the
-        supplied node and edge kinds are in their abbreviated form. prepend
-        specifies that the node and edge kinds are added to the beginning of
-        the metapath.
-        """
-        if not abbreviated:
-            node_kind = self.kind_to_abbrev[node_kind]
-            edge_kind = self.kind_to_abbrev[edge_kind]
-        if prepend:
-            abbrev = node_kind + edge_kind + metapath.abbrev
-        else:
-            abbrev = metapath.abbrev + edge_kind + node_kind
-        return self.metapath_from_abbrev(abbrev)
-        
 
 class MetaPath(object):
     
-    def __init__(self, metapaths, kind_tuple):
-        self.metapaths = metapaths
-        self.tuple_ = kind_tuple
-        self.abbrev = ''.join(metapaths.kind_to_abbrev[kind] for kind in kind_tuple)
-        assert self.abbrev not in metapaths.abbrev_to_path
-        metapaths.abbrev_to_path[self.abbrev] = self
-    
-    def start(self):
-        """Return the node_kind for the start node of the metapath."""
-        return self.tuple_[0]
+    tuple_to_metapath = dict()
+    kind_to_abbrev = dict()
 
-    def end(self):
-        """Same as start except returns the node_kind for the end node."""
-        return self.tuple_[-1]
+    def __new__(cls, kind_tuple):
+        """
+        If a MetaPath object representing kin_tuple exists, return that
+        object. Otherwise create and return a new MetaPath object.
+        """
+
+        if kind_tuple in MetaPath.tuple_to_metapath:
+            return MetaPath.tuple_to_metapath[kind_tuple]
+
+        new = object.__new__(cls)
+        new.tuple_ = kind_tuple
+        new.hash_ = hash(kind_tuple)
+        #new.abbrev = new.get_abbrev()
+        MetaPath.tuple_to_metapath[kind_tuple] = new
+        return new
+
+    @staticmethod
+    def edges_in_metapaths(metapaths):
+        """Returns a set of edges appearing in any of the metapaths"""
+        edges = set()
+        for metapath in metapaths:
+            edges |= set(metapath.get_edges())
+        return edges    
+
+    @staticmethod
+    def shortcuts_for_metapaths(metapaths, shortcut_length):
+        """Compute desired shortcuts for faster computation of metapaths."""
+        shortcuts = set()
+        for metapath in metapaths:
+            shortcuts |= metapath.shortcuts(shortcut_length)
+        return shortcuts
+
+    def shortcuts(self, length):
+        shorcuts = set()
+        num_nodes = len(self)
+        depth = 0
+        while depth + length <= num_nodes:
+            start = depth * 2
+            end = start + length * 2 + 1
+            shortcut_tuple = self[start: end]
+            shortcut = MetaPath(shortcut_tuple)
+            shorcuts.add(shortcut)
+            depth += length
+        return shorcuts
     
     def get_edges(self):
-        """Return a the set of edges composing the metapath."""
+        """Return a the list of edges composing the metapath."""
         if hasattr(self, 'edges'):
             return self.edges
         
-        edges = set()
+        edges = list()
         for depth in xrange(len(self)):
-            edge = self.tuple_[depth * 2 : depth * 2 + 3]
+            edge = self[depth * 2 : depth * 2 + 3]
             edge = edge[0], edge[2], edge[1]
-            edges.add(edge)
+            edges.append(edge)
         
         self.edges = edges
         return self.edges
+    
+    def get_abbrev(self):
+        """Return the abbreviation representing the metapath"""
+        if not hasattr(self, 'abbrev'):        
+            abbrev_generator = (MetaPath.kind_to_abbrev[kind] for kind in self)
+            self.abbrev = str.join('', abbrev_generator)
+        return self.abbrev
+    
+    def start(self):
+        """Return the node_kind for the start node of the metapath."""
+        return self[0]
+
+    def end(self):
+        """Same as start except returns the node_kind for the end node."""
+        return self[-1]
+    
+    def append(self, edge_kind, node_kind, prepend=False):
+        """
+        Return the metapath representing the supplied metapath appended with
+        the supplied edge_kind and node_kind. prepend specifies that the node
+        and edge kinds are added to the beginning of the metapath.
+        """
+        if prepend:
+            kind_tuple = (node_kind, edge_kind) + self.tuple_
+        else:
+            kind_tuple = self.tuple_ + (edge_kind, node_kind)
+        return MetaPath(kind_tuple)
     
     def split_by_index(self, index, reverse_head = False):
         """Split the metapath into two metapaths. Index is included as the 
         end node kind of head and the start node kind of tail.
         """
-        head = self.metapaths.metapath_from_tuple(self.tuple_[: index * 2 + 1])
-        tail = self.metapaths.metapath_from_tuple(self.tuple_[index * 2: ])
+        head = MetaPath(self[: index * 2 + 1])
+        tail = MetaPath(self[index * 2: ])
         if reverse_head:
             head = reversed(head)
         return head, tail
@@ -217,22 +210,28 @@ class MetaPath(object):
         return split_tuples
     
     def __reversed__(self):
-        return self.metapaths.metapath_from_tuple(reversed(self.tuple_))
+        return MetaPath(reversed(self))
+    
+    def __getitem__(self, index):
+        return self.tuple_[index]
+    
+    def __iter__(self):
+        return iter(self.tuple_)
     
     def __len__(self):
         return len(self.tuple_) / 2
     
     def __hash__(self):
-        return hash(self.abbrev)
+        return self.hash_
     
     def __eq__(self, other):
-        return self.abbrev == other.abbrev
+        return self.tuple_ == other.tuple_
     
     def __str__(self):
         return self.__repr__()
 
     def __repr__(self):
-        return self.abbrev
+        return self.get_abbrev()
 
 if __name__ == '__main__':
     
@@ -252,13 +251,14 @@ if __name__ == '__main__':
     
     source = 'drug'
     target = 'disease'
-    max_length = 5
+    max_length = 3
     
     print 'Metapaths'
     metapaths = extract_metapaths(schema, source, target, max_length)
-    shortcuts = shortcuts_for_metapaths(schema, metapaths, 2)
     print metapaths
+
     print 'Shortcuts'
+    shortcuts = MetaPath.shortcuts_for_metapaths(metapaths, 2)
     print shortcuts
     
     print 'Metapaths Excluding source to target edges'
@@ -275,8 +275,8 @@ if __name__ == '__main__':
     
     
     print metapaths[2].get_edges()
-    print metapaths[2].split(0)
-    print metapaths[2].split(1)
+    print metapaths[2].split_by_index(0)
+    print metapaths[2].split_by_index(1)
     
     """
     edge_tuples = [('drug', 'gene', 'upregulates'),
@@ -285,6 +285,4 @@ if __name__ == '__main__':
                    ('disease', 'gene', 'downregulates'),
                    ('gene', 'gene', 'function')]
     """
-    
-    
     
