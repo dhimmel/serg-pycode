10bd5f8aa411dcdf3c563780c5c879afb40c4517
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2013-05-23 01:26:21 -0700

    Basic processing of TIGER expression results complete.

--- bioparser/tiger.py
+++ bioparser/tiger.py
@@ -1,11 +1,13 @@
 import os
 import csv
 
+import data
+
 class TIGER(object):
     
     def __init__(self, tiger_dir=None):
         if tiger_dir is None:
-            tiger_dir = data.current_path('tiger')
+            tiger_dir = data.source_data_dir('tiger')
         self.tiger_dir = tiger_dir
 
     def get_tissues(self):
@@ -17,14 +19,43 @@ class TIGER(object):
             self.tissues = tissues
         return self.tissues
     
-    def get_hs_to_symbol(self):
-        if not hasattr(self, 'hs_to_symbol'):
+    def get_unigene_to_symbol(self):
+        if not hasattr(self, 'unigene_to_symbol'):
             path = os.path.join(self.tiger_dir, 'symbol2hs-Table.txt')
             unigene_to_symbol = dict()
             with open(path) as f:
-                reader = csv.DictReader(f, delimiter='\t')
+                reader = csv.reader(f, delimiter='\t')
+                reader.next() # header
                 for row in reader:
-                    row['Gene_symbol']
-                    row['UniGene(s)']
+                    symbol = row[0]
+                    unigenes = row[1:]
+                    for unigene in unigenes:
+                        unigene_to_symbol[unigene] = symbol
+            self.unigene_to_symbol = unigene_to_symbol
+        return self.unigene_to_symbol
 
-            self.tissues = tissues
+    def get_gene_to_tissues(self):
+        """ """
+        if not hasattr(self, 'gene_to_tissues'):
+            unigene_to_symbol = self.get_unigene_to_symbol()
+            path = os.path.join(self.tiger_dir, 'hs2tissue-Table.txt')
+            with open(path) as f:
+                reader = csv.reader(f, delimiter='\t')
+                reader.next() # header
+                gene_to_tissues = dict()
+                for row in reader:
+                    unigene = row[0]
+                    symbol = unigene_to_symbol.get(unigene)
+                    if symbol is None:
+                        continue
+                    tissues = row[1:]
+                    for tissue in tissues:
+                        gene_to_tissues.setdefault(symbol, set()).add(tissue)
+            self.gene_to_tissues = gene_to_tissues
+        return self.gene_to_tissues
+
+    
+
+tiger = TIGER()
+tiger.get_unigene_to_symbol()
+print tiger.get_gene_to_tissues().items()[:40]
