567b46059de9a55c02ecdd55c71a7e2ef9309c1f
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2013-08-29 17:00:56 -0700

    Added readwrite.features code

--- hetnet/readwrite/features.py
+++ hetnet/readwrite/features.py
@@ -1,1 +1,27 @@
+import csv
+import collections
+import ast
+
+
+def write_text(features, path):
+    """ """
+    fieldnames = ['name', 'algorithm', 'arguments']
+    with open(path, 'w') as write_file:
+        writer = csv.DictWriter(write_file, delimiter='\t', fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(features)
+
+def read_text(path):
+    """ """
+    features = list()
+    with open(path) as readfile:
+        reader = csv.DictReader(readfile, delimiter='\t')
+        for row in reader:
+            feature = collections.OrderedDict()
+            feature['name'] = row['name']
+            feature['algorithm'] = row['algorithm']
+            feature['arguments'] = ast.literal_eval(row['arguments'])
+            features.append(feature)
+    return features
+
 
