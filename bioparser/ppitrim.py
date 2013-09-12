66efe2d2be43afa6de5161644b585bdc21f0a60b
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2013-09-12 10:18:26 -0700

    Additional work on ppitrim.

--- bioparser/ppitrim.py
+++ bioparser/ppitrim.py
@@ -30,14 +30,14 @@ class PPITrim(object):
             fieldnames.append('negative')
         reader = csv.DictReader(read_file, delimiter='\t', fieldnames=fieldnames)
         plural_fields = ['uidA', 'uidB', 'confidence']
-        multiplex_fields = ['altA', 'altB', 'aliasA', 'aliasB', 'pmids']
+        multiplex_fields = ['altA', 'altB', 'aliasA', 'aliasB', 'pmids', 'interactionIdentifier']
         for row in reader:
                         
             # split all fields into elements
             for fieldname, field in row.items():
                 if field != '-':
                     row[fieldname] = field.split('|')
-            
+                        
             # fields where each element contains a unique id
             for fieldname in plural_fields:
                 field = row[fieldname]
@@ -58,7 +58,9 @@ class PPITrim(object):
                     continue
                 field_dict = dict()
                 for elem in field:
-                    key, value = elem.split(':')
+                    if elem == '-':
+                        continue
+                    key, value = elem.split(':', 1) # go:GO:0005783 requires split(':', 1)
                     field_dict.setdefault(key, list()).append(value)
                 row[fieldname] = field_dict
             
@@ -76,12 +78,21 @@ class PPITrim(object):
                 binary_rows.append(row)
         return complex_to_rows, binary_rows
     
+    
+    def get_interaction_info(self, row):
+        interaction_info = collections.OrderedDict()
+        interaction_info['pubmed'] = ', '.join(row['pmids'].get('pubmed', ''))
+        interaction_info['method'] = ', '.join(row['method'])
+        interaction_info['interaction_type'] = ', '.join(row['interactionType'])
+        interaction_info['edge_type'] = ', '.join(row['interactionIdentifier'].get('edgetype', list()))
+        interaction_info['sources'] = ', '.join(row['sourcedb'])
+        # all binary interactions are 'none' and complexes are 'bipartite' for expansion
+        #interaction_info['expansion'] = ', '.join(row['expansion'])
+        return interaction_info
+
     def binary_rows_to_interactions(self, binary_rows):
         for row in binary_rows:
-            interaction_info = collections.OrderedDict()
-            interaction_info['pubmed'] = ', '.join(row['pmids'].get('pubmed', ''))
-            interaction_info['method'] = ', '.join(row['method'])
-            interaction_info['sources'] = ', '.join(row['sourcedb'])
+            interaction_info = self.get_interaction_info(row)
             aliases_a = row['aliasA']['entrezgene/locuslink']
             aliases_b = row['aliasB']['entrezgene/locuslink']
             symbol_pairs = itertools.product(aliases_a, aliases_b)
@@ -92,6 +103,24 @@ class PPITrim(object):
                 interaction.update(interaction_info)
                 yield interaction
     
+    def complexes_to_interactions(self, complex_to_rows):
+        """ """
+        for complex, rows in complex_to_rows.iteritems():
+            interaction_info = self.get_interaction_info(rows[0])
+            aliases = set()
+            for row in rows:
+                aliases_b = row['aliasB']['entrezgene/locuslink']
+                aliases |= set(aliases_b)
+            symbol_pairs = itertools.combinations(aliases, 2) # matrix model
+            for symbol_a, symbol_b in symbol_pairs:
+                #interaction_info_temp = get_interaction_info(row)
+                #assert interaction_info_temp.values() == interaction_info.values()
+                interaction = collections.OrderedDict()
+                interaction['source'] = symbol_a
+                interaction['target'] = symbol_b
+                interaction.update(interaction_info)
+                yield interaction
+
     def interaction_hgnc_convert(self, interactions, exclude_self_references=True):
         hgnc = data.Data().hgnc
         symbol_to_gene = hgnc.get_symbol_to_gene()
@@ -107,19 +136,23 @@ class PPITrim(object):
             interaction['target'] = target
             yield interaction
             
-    
+    def all_interactions(self):
+        complex_to_rows, binary_rows = self.separate_complexes()
+
+        complex_interactions = self.complexes_to_interactions(complex_to_rows)
+        complex_interactions = self.interaction_hgnc_convert(complex_interactions)
+        
+        binary_interactions = self.binary_rows_to_interactions(binary_rows)
+        binary_interactions = self.interaction_hgnc_convert(binary_interactions)
+        
+        complex_interactions = list(complex_interactions)
+        binary_interactions = list(binary_interactions)
+        
+        return complex_interactions, binary_interactions
+
     
 if __name__ =='__main__':
     ppi = PPITrim()
-    complex_to_rows, binary_rows = ppi.separate_complexes()
-    interactions = ppi.binary_rows_to_interactions(binary_rows)
-    interactions = ppi.interaction_hgnc_convert(interactions)
-    for interaction in interactions:
-        print interaction
-
-    #for interaction in interactions:
-    #    print interaction['pmids']
-    #    pubmed = interaction['pmids'].get('pubmed', list())
-    #    assert len(pubmed) <= 1
-    #    #print interaction['aliasA'], interaction['aliasB'], interaction['pmids'], interaction['confidence']['maxsources']
-    print len(complex_to_rows), len(binary_rows)
+    complex_interactions, binary_interactions = ppi.all_interactions()
+    print 'complex', len(complex_interactions)
+    print 'binary', len(binary_interactions)
--- scripts/ictnet2.py
+++ scripts/ictnet2.py
@@ -12,12 +12,14 @@ Are we excluding non-protein coding genes?
 """
 class TableWriter(object):
     
-    def __init__(self, table_name):
+    def __init__(self, table_name, fieldnames):
         """Class to write tab delimited text files encoding ictnet tables."""
         self.table_name = table_name
+        self.fieldnames = fieldnames
         self.path = os.path.join(ictnet_dir, 'tables', table_name + '.txt')
         self.file = open(self.path, 'w')
-        self.writer = csv.writer(self.file, delimiter='\t')
+        self.writer = csv.DictWriter(self.file, delimiter='\t', fieldnames=fieldnames)
+        self.writer.writeheader()
     
     def writerow(self, row):
         self.writer.writerow(row)
@@ -25,7 +27,7 @@ class TableWriter(object):
     def writerows(self, rows, sort=True):
         if sort:
             rows = list(rows)
-            rows.sort()
+            rows.sort(key=lambda row: [row[key] for key in self.fieldnames])
         for row in rows:
             self.writerow(row)
         
@@ -44,43 +46,46 @@ class TableWriter(object):
 ############# genes
 hgnc = bioparser.data.Data().hgnc
 symbol_to_gene = hgnc.get_symbol_to_gene()
-tb_gene_rows = list() # (gene_id, symbol, name, chromosome)
-tb_gene_alias_rows = list() # (gene_id, alias)
+tb_gene_fieldnames = ('gene_id', 'symbol', 'name', 'chromosome')
+tb_gene_rows = list()
+tb_gene_alias_fieldnames = ('gene_id', 'alias')
+tb_gene_alias_rows = list()
 genes = hgnc.get_genes()
 for gene in genes:
     gene_id = gene.id_
-    row = (gene_id, gene.symbol, gene.name, gene.chromosome)
+    row = {'gene_id':gene_id, 'symbol': gene.symbol,
+           'name': gene.name, 'chromosome': gene.chromosome}
     tb_gene_rows.append(row)
     for alias in gene.synonyms + gene.previous_symbols:
-        tb_gene_alias_rows.append((gene_id, alias))
+        row = {'gene_id': gene_id, 'alias': alias}
+        tb_gene_alias_rows.append(row)
 
 # write tb_gene_rows
-with TableWriter('tb_gene') as writer:
-    writer.writerow(('gene_id', 'symbol', 'name', 'chromosome'))
+with TableWriter('tb_gene', tb_gene_fieldnames) as writer:
     writer.writerows(tb_gene_rows)
 
 # write tb_gene_alias_rows
-with TableWriter('tb_gene_alias') as writer:
-    writer.writerow(('gene_id', 'alias'))
+with TableWriter('tb_gene_alias', tb_gene_alias_fieldnames) as writer:
     writer.writerows(tb_gene_alias_rows)
 
 ################################################################################
 ############# ppi
-tb_ppi_rows = list() #(source, taget, pubmed, method, sources)
+tb_ppi_rows = list() #(source, taget, pubmed, method, sources, complex)
+tb_ppi_fieldnames = ('source', 'taget', 'pubmed', 'method', 'interaction_type',
+                     'edge_type', 'sources', 'complex')
 
 ppitrim = bioparser.data.Data().ppitrim
-complex_to_rows, binary_rows = ppitrim.separate_complexes()
-interactions = ppitrim.binary_rows_to_interactions(binary_rows)
-interactions = ppitrim.interaction_hgnc_convert(interactions)
-for ppi in interactions:
-    source_id = ppi['source'].id_
-    target_id = ppi['target'].id_
-    row = (source_id, target_id, ppi['pubmed'], ppi['method'], ppi['sources'])
-    tb_ppi_rows.append(row)
+complex_interactions, binary_interactions = ppitrim.all_interactions()
+for is_complex, ppis in enumerate([binary_interactions, complex_interactions]):
+    for ppi in ppis:
+        ppi['source'] = ppi['source'].id_
+        ppi['target'] = ppi['target'].id_
+        ppi['complex'] = is_complex
+        tb_ppi_rows.append(row)
 
 # write tb_ppi_rows
 with TableWriter('tb_ppi') as writer:
-    writer.writerow(('source', 'taget', 'pubmed', 'method', 'sources'))
+    writer.writerow(('source', 'taget', 'pubmed', 'method', 'sources', 'complex'))
     writer.writerows(tb_ppi_rows)
 
 ################################################################################
